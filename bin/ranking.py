aae34e324d61931636c05428686a0f4561e142c3
Raphaël Vinot <raphael@vinot.info>
2018-11-26 14:59:03 +0100

    fix: Wrong variable name

--- bin/ranking.py
+++ bin/ranking.py
@@ -21,5 +21,5 @@ class RankingManager(AbstractManager):
 
 
 if __name__ == '__main__':
-    dbinsert = RankingManager()
-    dbinsert.run(sleep_in_sec=3600)
+    ranking = RankingManager()
+    ranking.run(sleep_in_sec=3600)
