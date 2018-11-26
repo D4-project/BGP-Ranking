89796618a087b17051fe6c70adf22632254fa7d0
Raphaël Vinot <raphael@vinot.info>
2018-11-26 10:35:22 +0100

    fix: Skip non existing feeds in bambenekconsulting list

--- bgpranking/parsers/bambenekconsulting.py
+++ bgpranking/parsers/bambenekconsulting.py
@@ -9,5 +9,8 @@ from typing import List
 
 
 def parse_raw_file(self, f: BytesIO) -> List[bytes]:
+    if re.findall(b'This feed is not generated for this family', f.getvalue()):
+        return []
+
     self.datetime = parse(re.findall(b'## Feed generated at: (.*)\n', f.getvalue())[0])
     return self.extract_ipv4(f.getvalue())
