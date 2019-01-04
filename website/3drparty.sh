794e050c1623c6785dcfd855a7ef3a5ad6723983
Raphaël Vinot <raphael@vinot.info>
2019-01-04 12:07:10 +0100

    chg: Bump bootstrap-select

--- website/3drparty.sh
+++ website/3drparty.sh
@@ -7,7 +7,7 @@ mkdir -p web/static/
 
 wget https://d3js.org/d3.v5.js -O web/static/d3.v5.js
 
-BOOTSTRAP_SELECT="1.12.4"
+BOOTSTRAP_SELECT="1.13.5"
 
 wget https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/${BOOTSTRAP_SELECT}/css/bootstrap-select.min.css -O web/static/bootstrap-select.min.css
 wget https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/${BOOTSTRAP_SELECT}/js/bootstrap-select.min.js -O web/static/bootstrap-select.min.js
