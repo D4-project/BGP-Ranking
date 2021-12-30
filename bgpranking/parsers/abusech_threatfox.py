19f1aa2a1a3979e3661393d3f73e0c533e90312c
Raphaël Vinot <raphael@vinot.info>
2021-12-30 11:28:03 +0100

    fix: missing variable in threatfox parser

--- bgpranking/parsers/abusech_threatfox.py
+++ bgpranking/parsers/abusech_threatfox.py
@@ -3,11 +3,13 @@
 
 import json
 
+from datetime import datetime
 from io import BytesIO
 from typing import List
 
 
 def parse_raw_file(self, f: BytesIO) -> List[str]:
+    self.datetime = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
     to_return = []
     for entry in json.loads(f.getvalue().decode()).values():
         ip_port = entry[0]['ioc_value']
